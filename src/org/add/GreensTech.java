67e4a7e37a20ab836837351243dbf95d0bd9ef3b
PavithranSai <pavithranpavi180797@gmail.com>
2023-01-11 09:50:48 +0530

    Address of Greens Tech

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,23 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("No:000, balaji nagar, OMR");
+	}
+	private void greensAdayar() {
+		System.out.println("No: 123, bharathiyar nagar, Adayar");
+	}
+	private void greensTambaram() {
+		System.out.println("No.456, Thilagar Nagar, Tambaram");
+	}
+	private void greensVelacherry() {
+		System.out.println("No:789, Kumaran Nagra, Velacherry");
+	}
+	public static void main(String[] args) {
+		GreensTech GreensTech = new GreensTech();
+		GreensTech.greensOmr();
+		GreensTech.greensAdayar();
+		GreensTech.greensTambaram();
+		GreensTech.greensVelacherry();
+	}
+}
